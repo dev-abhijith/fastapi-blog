979e55b2ca31c950eec934022d155022ab5d7815
dev-abhijith <abhijith2040@gmail.com>
2021-07-13 18:32:50 +0530

    added database

--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,24 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from typing import final
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm.session import Session
+from . import schema, models
+from .database import SessionLocal, engine
 
 app = FastAPI()
 
-class Blog(BaseModel):
-    title: str
-    body: str
+models.Base.metadata.create_all(engine)
 
-@app.post('/blog')
-def blog(request: Blog):
-    return request
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post('/blog')  
+def blog(request: schema.Blog, db:Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
